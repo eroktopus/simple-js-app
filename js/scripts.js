40c51a4923e6de1049882d12689b562f16d45565
eroktopus <147647841+eroktopus@users.noreply.github.com>
2023-12-05 16:54:56 -0500

    Update scripts.js
    started pokedex. Added first 4 pokemon and 3 attributes for each

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('hello world');
-let favoriteFood = 'tacos';
-document.write (favoriteFood);
+let pokemonList=[
+    {name: 'bulbasaur', height: 7, type: ['poison','grass']},
+    {name: 'squirtle', height: 9, type: 'water'},
+    {name: 'charmander', height: 12, type: 'fire'},
+    {name: 'evee', height: 8, type: 'normal'},
+];
