bfb5f7a0f08ec2597a0326a3d6b7c157fa64d3ff
eroktopus <147647841+eroktopus@users.noreply.github.com>
2023-12-14 13:48:52 -0500

    wrote pokemon list to DOM, checked errors, added loop,
    used document.write pokemonList to DOM, used a loop to notate large pokemon,

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
 let pokemonList=[
-    {name: 'bulbasaur', height: 7, type: ['poison','grass']},
-    {name: 'squirtle', height: 9, type: 'water'},
-    {name: 'charmander', height: 12, type: 'fire'},
-    {name: 'evee', height: 8, type: 'normal'},
-];
+    {name: 'Bulbasaur', height: 7, type: ['poison', 'grass']},
+    {name: 'Squirtle', height: 9, type: 'water'},
+    {name: 'Charmander', height: 12, type: 'fire'},
+    {name: 'Evee', height: 8, type: 'normal'},
+      ];
+
+
+
+      for (let i = 0; i < pokemonList.length; i++) 
+{
+    document.write('<p ' + pokemonList[i].name + '</p>' +
+                   'Name: ' + pokemonList[i].name + '<br>' +
+                   'Height: ' + pokemonList[i].height + '<br>' +
+                   'Type: ' + pokemonList[i].type + '<br>');
+                   if (pokemonList[i].height >10) {
+                    document.write ("Wow, that's big!"); 
+                  }
+  
+
+  //                 for (let i = 0; i < pokemonList.length; i++) 
+  // if (pokemonList[i].height >10) {
+  //   document.write (pokemonList[i].height + "Wow, that's big!"); 
+}
+
+
+
+
+
+
