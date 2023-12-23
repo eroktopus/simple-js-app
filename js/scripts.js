bd2d6b4a94eaadd2e35c3cb464221acc7201b733
eroktopus <147647841+eroktopus@users.noreply.github.com>
2023-12-23 16:45:44 -0500

    for each loops
    switched from for loops to for each loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,68 @@
-let pokemonList=[
-    {name: 'Bulbasaur', height: 7, type: ['poison', 'grass']},
-    {name: 'Squirtle', height: 9, type: 'water'},
-    {name: 'Charmander', height: 12, type: 'fire'},
-    {name: 'Evee', height: 8, type: 'normal'},
-      ];
+// let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: ' Bulbasaur',
+      height: 7,
+      type: [' Poison', ' Grass']
+    },
+    {
+      name: ' Squirtle', 
+      height: 9, 
+      type: ' Water'
+    },
+    {
+      name: ' Charmander', 
+      height: 12, 
+      type: ' Fire'
+    },
+    {
+      name: ' Evee', 
+      height: 8, 
+      type: ' Normal'
+    }
+    ];
+  //   return {
+  //     add: function(pokemon) {
+  //       pokemonList.push(pokemon);
+  //     },
+  //     getAll: function() {
+  //       return pokemonList;
+  //     }
+  //   };
+  // })(); 
+pokemonList.forEach(function(pokemon) {
+  document.write('Name:' + pokemon.name + '<br>' + 'Height:' + pokemon.height + '<br>' + 'Type:' + pokemon.type + '<br>');
+  if (pokemon.height >10) {
+    document.write ("Wow, that's big!" + '<br>'); 
+  }
+});
 
 
 
-      for (let i = 0; i < pokemonList.length; i++) 
-{
-    document.write('<p ' + pokemonList[i].name + '</p>' +
-                   'Name: ' + pokemonList[i].name + '<br>' +
-                   'Height: ' + pokemonList[i].height + '<br>' +
-                   'Type: ' + pokemonList[i].type + '<br>');
-                   if (pokemonList[i].height >10) {
-                    document.write ("Wow, that's big!"); 
-                  }
-  
-
-  //                 for (let i = 0; i < pokemonList.length; i++) 
-  // if (pokemonList[i].height >10) {
-  //   document.write (pokemonList[i].height + "Wow, that's big!"); 
-}
 
 
 
 
+
+
+// let pokemonList=[
+//     {name: 'Bulbasaur', height: 7, type: ['poison', 'grass']},
+//     {name: 'Squirtle', height: 9, type: 'water'},
+//     {name: 'Charmander', height: 12, type: 'fire'},
+//     {name: 'Evee', height: 8, type: 'normal'},
+//       ];
+
+
+
+//       for (let i = 0; i < pokemonList.length; i++) 
+// {
+//     document.write('<p ' + pokemonList[i].name + '</p>' +
+//                    'Name: ' + pokemonList[i].name + '<br>' +
+//                    'Height: ' + pokemonList[i].height + '<br>' +
+//                    'Type: ' + pokemonList[i].type + '<br>');
+                  //  if (pokemonList[i].height >10) {
+                  //   document.write ("Wow, that's big!"); 
+                  // }
+// }
 
 
