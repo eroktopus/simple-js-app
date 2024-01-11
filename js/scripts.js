1102c527747ea47abc5bd4401a1e00e6afd0a8f6
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-01-11 15:56:56 -0500

    For each loop, IIFE, added functions
    Changed for loops to forEach loops, enclosed pokemonList inside an IIFE, added "add" and "getAll" functions, changed forEach loops to work with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,63 @@
-// let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: ' Bulbasaur',
-      height: 7,
-      type: [' Poison', ' Grass']
-    },
-    {
-      name: ' Squirtle', 
-      height: 9, 
-      type: ' Water'
-    },
-    {
-      name: ' Charmander', 
-      height: 12, 
-      type: ' Fire'
-    },
-    {
-      name: ' Evee', 
-      height: 8, 
-      type: ' Normal'
-    }
-    ];
-  //   return {
-  //     add: function(pokemon) {
-  //       pokemonList.push(pokemon);
-  //     },
-  //     getAll: function() {
-  //       return pokemonList;
-  //     }
-  //   };
-  // })(); 
-pokemonList.forEach(function(pokemon) {
-  document.write('Name:' + pokemon.name + '<br>' + 'Height:' + pokemon.height + '<br>' + 'Type:' + pokemon.type + '<br>');
-  if (pokemon.height >10) {
-    document.write ("Wow, that's big!" + '<br>'); 
-  }
+//IIFE
+let pokemonRepository = (function () {
+let pokemonList = [
+{
+  name: ' Bulbasaur',
+  height: 7,
+  type: [' Poison', ' Grass']
+},
+{
+  name: ' Squirtle', 
+  height: 9, 
+  type: ' Water'
+},
+{
+  name: ' Charmander', 
+  height: 12, 
+  type: ' Fire'
+},
+{
+  name: ' Evee', 
+  height: 8, 
+  type: ' Normal'
+}
+];
+function add(pokemon) {
+pokemonList.push(pokemon);
+}
+function getAll() {
+return pokemonList;
+}
+return {
+add: add,
+getAll: getAll
+}; 
+})(); 
+
+console.log(pokemonRepository.getAll());
+// add function
+pokemonRepository.add({  name: 'Meowth', 
+height: 6, 
+type: 'Normal'
+});
+// forEach loop
+pokemonRepository.getAll().forEach(function (pokemon) {
+if (pokemon.height >= 10) {
+  document.write("<div>" + "<p>" + "Name: " + pokemon.name+ ", " +  "Type: " + pokemon.type + ", " + 
+  "Height:" + " " + pokemon.height + " - Wow! That is a big pokemon! " + "</p>" + "</div>");
+} else if (pokemon.height) {
+  document.write("<div>" + "<p>" + "Name: " + pokemon.name + ", " +  "Type: " + pokemon.type + ", " + 
+  "Height:" + " " + pokemon.height + "  " + "</p>" + "</div>")
+}
 });
 
 
+// pokemonList.forEach(function(pokemon) {
+//   document.write('Name:' + pokemon.name + '<br>' + 'Height:' + pokemon.height + '<br>' + 'Type:' + pokemon.type + '<br>');
+//   if (pokemon.height >10) {
+//     document.write ("Wow, that's big!" + '<br>'); 
+//   }
+// });
 
 
 
@@ -45,24 +65,5 @@ pokemonList.forEach(function(pokemon) {
 
 
 
-// let pokemonList=[
-//     {name: 'Bulbasaur', height: 7, type: ['poison', 'grass']},
-//     {name: 'Squirtle', height: 9, type: 'water'},
-//     {name: 'Charmander', height: 12, type: 'fire'},
-//     {name: 'Evee', height: 8, type: 'normal'},
-//       ];
-
-
-
-//       for (let i = 0; i < pokemonList.length; i++) 
-// {
-//     document.write('<p ' + pokemonList[i].name + '</p>' +
-//                    'Name: ' + pokemonList[i].name + '<br>' +
-//                    'Height: ' + pokemonList[i].height + '<br>' +
-//                    'Type: ' + pokemonList[i].type + '<br>');
-                  //  if (pokemonList[i].height >10) {
-                  //   document.write ("Wow, that's big!"); 
-                  // }
-// }
 
 
