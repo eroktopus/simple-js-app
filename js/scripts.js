dbe14bc1611385b9a02fabd8d3b551c14017ce85
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-01-31 14:49:03 -0500

    added modal and dialogue button

--- js/scripts.js
+++ js/scripts.js
@@ -64,10 +64,102 @@ function loadList() {
     addListItem: addListItem,
     showDetails: showDetails
   };
-  // function addListItem(pokemon)
-})();
 
-// console.log(pokemonRepository.getAll());
+  function showModal(title, text) {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+    
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  function showDialog(title, text) {
+    showModal(title, text);
+  
+    //modalContainer defined
+    let modalContainer = document.querySelector('#modal-container');
+  
+    //add cancel button to modal
+    let modal = modalContainer.querySelector('.modal');
+  
+    let confirmButton = document.createElement('button');
+    confirmButton.classList.add('modal-confirm');
+    confirmButton.innerText = 'Confirm';
+  
+    let cancelButton = document.createElement('button');
+    cancelButton.classList.add('modal-cancel');
+    cancelButton.innerText = 'Cancel';
+  
+    modal.appendChild(confirmButton);
+    modal.appendChild(cancelButton);
+  
+    //focus on confirm button so user can just press enter
+    confirmButton.focus();
+    
+
+    return new Promise((resolve, reject) => {
+      cancelButton.addEventListener('click', hideModal);
+      confirmButton.addEventListener('click', () => {
+        dialogPromiseReject = null; // Reset this
+        hideModal();
+        resolve();
+      });
+      // This can be used to reject from other functions
+      dialogPromiseReject = reject;
+    });
+  }
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+  
+  document.querySelector('#show-dialog').addEventListener('click', () => {
+    showDialog('Confirm action', 'Are you sure you want to do this?').then(function() {
+      alert('confirmed!');
+    }, () => {
+      alert('not confirmed');
+    });
+  });
+
+
+  
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+  
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+})();
 
 // forEach loop 
 pokemonRepository.loadList().then(function () {
@@ -77,42 +169,83 @@ pokemonRepository.loadList().then(function () {
   });
 });
 
-// let button = document.querySelector('button');
-// button.addEventListener('click', function (event) {
-//   console.log(event);
-// });
+
+// let img = document.createElement("img"); //create an <img> element 
+// img.src = "img/pokemon-8.svg"; 
+// img.alt = "image description"; 
+// document.body.appendChild(img); 
 
 
+// (function() {
+//   let form = document.querySelector('#register-form');
+//   let emailInput = document.querySelector('#email');
+//   let passwordInput = document.querySelector('#password');
+// function showErrorMessage(input, message) {
+//   let container = input.parentElement; // The .input-wrapper
 
+//   // Check and Remove any existing errors
+//   let error = container.querySelector('.error-messge');
+//   if (error) {
+//     container.removeChild(error);
+//   }
+//   // Now add the error if the message isn't empty
+//   if (message){
+//     let error = document.createElement('div');
+//     error.classList.add('error-message');
+//     error.innerText = message;
+//     container.appendChild(error);
+//   }
+// }
+// function validateEmail() {
+//   let value = emailInput.value;
+ 
+//   if (!value) {
+//     showErrorMessage(emailInput, "Email is a required field." );
+//     return false;
+//   }
+//   if (value.indexOf('@')=== -1){
+//     showErrorMessage(emailInput, 'You must enter a valid email address.');
+//     return false;
+//   }
+//   if (value.indexOf('.') === -1){
+//     showErrorMessage(emailInput, 'You must enter a valid email address.');
+//     return false;
+//   }
 
-
-
-
-
-// {
-//   name: ' Bulbasaur',
-//   height: 7,
-//   type: [' Poison', ' Grass']
-// },
-// {
-//   name: ' Squirtle', 
-//   height: 9, 
-//   type: ' Water'
-// },
-// {
-//   name: ' Charmander', 
-//   height: 12, 
-//   type: ' Fire'
-// },
-// {
-//   name: ' Evee', 
-//   height: 8, 
-//   type: ' Normal'
-// },
-// {
-//   name: ' Mewtwo', 
-//   height: 11, 
-//   type: ' Psychic'
+//   showErrorMessage(emailInput, null);
+//   return true;
 // }
 
+// function validatePassword() {
+//   let value = passwordInput.value;
+ 
+//   if (!value) {
+//     showErrorMessage(passwordInput, 'Password is a required field.');
+//     return false;
+//   }
+//   if (value.length < 8) {
+//     showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
+//     return false;
+//   }
+  
+//   showErrorMessage(passwordInput, null);
+//   return true;
+// }
+
+// function validateForm() {
+//   let isValidEmail = validateEmail();
+//   let isValidPassword = validatePassword();
+//   return isValidEmail && isValidPassword;
+// }
+// form.addEventListener('submit' , (e) => {
+//   e.preventDefault(); //Do not submit to server
+//   if (validateForm()) {
+//     alert('Success!');
+//   }
+// })
+// emailInput.addEventListener('input', validateEmail);
+// passwordInput.addEventListener('input', validatePassword);
+
+// // THE RETURN STATEMENT HERE
+// })();
 
